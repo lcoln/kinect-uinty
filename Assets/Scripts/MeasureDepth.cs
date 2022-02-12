5d00ce8369333df0964a029a90857dc3493b4b24
lcoln <875482941@qq.com>
2022-02-12 18:44:01 +0800

    init project

---
+++ Assets/Scripts/MeasureDepth.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class MeasureDepth : MonoBehaviour
+{
+    public MultiSourceManager mMultiSource;
+    public Texture2D mDepthTexture;
+
+    private ushort[] mDepthData = null;
+    private CameraSpacePoint[] mCameraSpacePoints = null;
+    private ColorSpacePoint[] mColorSpacePoints = null;
+
+    private KinectSensor mSensor = null;
+    private CoordinateMapper mMapper = null;
+
+    private readonly Vector2Int mDepthResolution = new Vector2Int(512, 424);
+
+    private void Awake()
+    {
+        mSensor = KinectSensor.GetDefault();
+        mMapper = mSensor.CoordinateMapper;
+
+        int arraySize = mDepthResolution.x * mDepthResolution.y;
+
+        mCameraSpacePoints = new CameraSpacePoint[arraySize];
+        mColorSpacePoints = new ColorSpacePoint[arraySize];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.space)) {
+            DepthToDepth();
+
+            mDepthTexture = CreateTexture();
+        }
+    }
+
+    private void DepthToDepth
+    {
+        mDepthData = mMultiSource.GetDepthData();
+
+        mMapper.MapDepthFrameToCameraSpace(mDepthData, mCameraSpacePoints);
+        mMapper.MapDepthFrameToColorSpace(mDepthData, mColorSpacePoints);
+    }
+
+    private Texture2D CreateTexture()
+    {
+        Texture2D newTexture = new Texture2D(1920, 1080, TextureFormat.Alpha8, false);
+
+        for (int x = 0;x < 1920;x++)
+        {
+            for (int y = 0;y < 1080;y++)
+            {
+                newTexture.setPixel(x, y, Color.clear);
+            }
+        }
+
+        foreach(ColorSpacePoint point in mColorSpacePoints)
+        {
+            newTexture.setPixel((int)point.x, (int)point.y, Color.black);
+        }
+
+        return newTexture;
+    }
+}
